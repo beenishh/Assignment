6f8e8c244bc439a7458155a30429211530d9f154
beenishh <78582613+beenishh@users.noreply.github.com>
2021-02-27 22:50:14 +0530

    step 4 done

--- brain_image.py
+++ brain_image.py
@@ -12,7 +12,11 @@ brain_image=plt.imread("Brain.jpg")
 plt.imshow(brain_image)
 plt.title('Display Image read using imread()') 
 plt.axis('off')
-plt.show()
+plt.show()    # show the image as output
 
 # find the dimension of the image
 brain_image.shape
+
+# use the reverse greyscale colormap 
+plt.imshow(brain_image, cmap='Greys_r')
+plt.show()    # show the image as output
