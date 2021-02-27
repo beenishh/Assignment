2966b7f6860617c7bf41d52a09b87f0509b42856
beenishh <78582613+beenishh@users.noreply.github.com>
2021-02-27 22:45:10 +0530

    done with step 2 of the assignment

--- brain_image.py
+++ brain_image.py
@@ -4,3 +4,12 @@ cd /mnt/c/Users/beenish/Downloads/
 conda activate image_processing 
 python
 
+# read the image into a numpy.ndarray
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+brain_image=plt.imread("Brain.jpg") 
+plt.imshow(brain_image)
+plt.title('Display Image read using imread()') 
+plt.axis('off')
+plt.show()
